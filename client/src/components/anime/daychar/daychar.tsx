8b4167a45efd32f2893754ea983c0b1940a4fa3f
Randy Calderon <randycweb@gmail.com>
2019-05-13 18:15:51 -0700

    Changes to await used in character fetching and added position styling for character container

--- client/src/components/anime/daychar/daychar.tsx
+++ client/src/components/anime/daychar/daychar.tsx
@@ -18,34 +18,47 @@ interface Data {
 const DayChar: React.FC = () => {
   const [character, setCharacter] = useState<Data[]>([]);
 
+  // Look into refetch issue and handling issues with randomCharacter request or find other api to query from to prevent issues
   useEffect(() => {
-    request.randomCharacter().then((res: any) => {
-      setCharacter([
-        {
-          about: res.about,
-          animeography: res.animeography,
-          image_url: res.image_url,
-          mangaography: res.mangaography,
-          name: res.name,
-          name_kanji: res.name_kanji,
-          voice_actors: res.voice_actors,
-        },
-      ]);
-    });
+    const fetchData = async () => {
+      try {
+        const result = await request.randomCharacter();
+        if (result.animeography.length === 0 || result.length === 0) {
+          result();
+        } else {
+          setCharacter([
+            {
+              about: result.about,
+              animeography: result.animeography,
+              image_url: result.image_url,
+              mangaography: result.mangaography,
+              name: result.name,
+              name_kanji: result.name_kanji,
+              voice_actors: result.voice_actors,
+            },
+          ]);
+        }
+      } catch {}
+    };
+    fetchData();
   }, []);
 
+  console.log(character);
   return (
     <div className={daychar.container}>
       <h2 className={daychar.header}>Character of the Day</h2>
       {character.map(data => {
         return (
-          <div className={daychar.charContainer}>
+          <React.Fragment>
             <img
+              height="220"
+              width="180"
               className={daychar.charImg}
               src={data.image_url}
               alt={`Picture of the anime character ${data.name}`}
             />
-          </div>
+            <span className={daychar.charspan}>{data.name}</span>
+          </React.Fragment>
         );
       })}
     </div>
